707517146704af6c7c57ebc3f193b6f565583589
Kandalova Elena <elena@kandalova.ru>
2019-10-23 16:15:41 +0300

    ДЗ урок 1: со scrollView и картинкой на фоне

---
+++ UserInterface_KandalovaElena/UserInterface_KandalovaElena/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserInterface_KandalovaElena
+//
+//  Created by Elena Kandalova on 23/10/2019.
+//  Copyright © 2019 Elena Kandalova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
